e5ca2477ec3d4f5dc5a1e7fbbfa19fdcbd421db4
Maira <maaottoni@gmail.com>
2023-01-31 14:31:30 -0300

    Atualiza função piores desempenhos

--- piores_desempenhos_isf/2022Q3/piores_desempenhos.py
+++ piores_desempenhos_isf/2022Q3/piores_desempenhos.py
@@ -1,39 +1,19 @@
 import pandas as pd
 
-setembro = r'piores_desempenhos_isf\2022Q3\202209-setembro.csv'
-outubro = r'piores_desempenhos_isf\2022Q3\202212-outubro.csv'
-novembro = r'piores_desempenhos_isf\2022Q3\202211-novembro.csv'
-dezembro = r'piores_desempenhos_isf\2022Q3\202212-dezembro.csv'
+caminho = r'piores_desempenhos_isf\2022Q3\2022Q3.csv'
 
-set2022 = pd.read_csv(setembro)
-set2022 = set2022.rename(columns={'isf_nota':'isf_setembro'})
-set2022 = set2022[['municipio_id_sus','uf_sigla','municipio_nome','isf_setembro']]
+df = pd.read_csv(caminho)
+df['nota'] = (df['nota_porcentagem']/df['meta'])*10
+df.loc[df['nota']>10,'nota']=10
+#df.to_csv(r'piores_desempenhos_isf\2022Q3\isf_2022Q3.csv')
+#print(df)
 
-out2022 = pd.read_csv(outubro)
-out2022 = out2022.rename(columns={'isf_nota':'isf_outubro'})
-out2022 = out2022[['municipio_id_sus','isf_outubro']]
+isf = r'piores_desempenhos_isf\2022Q3\isf_2022Q3_corrigir.csv'
+df_isf = pd.read_csv(isf)
+df_isf = df_isf.drop(columns=['peso','meta','nota','indicadores_nome','resultado','isf_calculado','isf_calculado_erro']).dropna().reset_index(drop=True)
+df_isf['isf_calculado_final'] = df_isf['isf_calculado_final'].round(2)
+df_isf = df_isf.sort_values(by='isf_calculado_final', ascending=True).reset_index(drop=True)
+df_100 = df_isf.iloc[:100]
+#df_100.to_csv(r'piores_desempenhos_isf\2022Q3\piores_desempenhos_2022Q3.csv')
 
-nov2022 = pd.read_csv(novembro)
-nov2022 = nov2022.rename(columns={'isf_nota':'isf_novembro'})
-nov2022 = nov2022[['municipio_id_sus','isf_novembro']]
-
-
-dez2022 = pd.read_csv(dezembro)
-dez2022 = dez2022.rename(columns={'isf_nota':'isf_dezembro'})
-dez2022 = dez2022[['municipio_id_sus','isf_dezembro']]
-
-
-
-
-df = set2022.merge(out2022, how='outer', on='municipio_id_sus')
-df = df.merge(nov2022,how='outer',on='municipio_id_sus')
-df = df.merge(dez2022,how='outer',on='municipio_id_sus')
-df['media'] = (df['isf_setembro'] + df['isf_outubro'] + df['isf_novembro'] + df['isf_dezembro'])/4
-df['municipio_uf'] = df['municipio_nome'] + '-' + df['uf_sigla']
-df = df.sort_values(by='media', ascending=True).reset_index(drop=True)
-df=df[['municipio_id_sus', 'uf_sigla','municipio_uf', 'isf_setembro',  'isf_outubro',  'isf_novembro',  'isf_dezembro',   'media' ]]
-df_100 = df.iloc[:100]
-
-#df = df[['municipio_id_sus','uf_sigla_x','municipio_nome_x','isf_setembro','isf_outubro']]
-
-print(df_100)
+print(df_100)   
